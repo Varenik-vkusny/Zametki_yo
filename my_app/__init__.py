24ac6cc4ca9954f8f38d4e4933117a2c480776cf
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-06-17 00:17:58 +0500

    Initial project structure and a little garbage

---
+++ my_app/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+migrate = Migrate()
+
+def create_app():
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY = ""
+        SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+    csrf.init_app(app)
+    migrate.init_app(app, db)
+
+    from .main
